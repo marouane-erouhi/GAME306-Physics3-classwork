4cbad08b6363a4c818322432f4964bb4e75ade51
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2024-09-12 14:43:48 -0400

    redid Cylinder to simplify it a bit.

--- ComponentFramework/Cylinder.cpp
+++ ComponentFramework/Cylinder.cpp
@@ -30,13 +30,14 @@ void GEOMETRY::Cylinder::generateVerticesAndNormals() {
 	const float deltaPhi = 10.0f;
 
 	float distanceBetweenCaps = VMath::distance(capCenterPosA, capCenterPosB);
-	int numberOfCircles = 5; // how many circles between them?
+	int numberOfCircles = 20; // how many circles between them?
 	float distanceBetweenCircles = distanceBetweenCaps / numberOfCircles;
 
 	Vec3 dir = VMath::normalize(capCenterPosB - capCenterPosA); // Axis direction
-	Vec3 circle(r, 0.0f, 0.0f); // Initial point on the cap
+	//Vec3 circle(r, 0.0f, 0.0f); // Initial point on the cap
 	Vec3 perp = perpendicular(dir); // Perpendicular vector to the axis
 
+/* i don't fully understand the use of perp vector
 	for (int i = 0; i < numberOfCircles; i++) {
 		for (float phiDeg = 0.0f; phiDeg <= 360.0f; phiDeg += deltaPhi) {
 			// Rotate around the cylinder's axis
@@ -50,6 +51,37 @@ void GEOMETRY::Cylinder::generateVerticesAndNormals() {
 			normals.push_back(VMath::normalize(rotatedPoint)); // Normal points outwards
 		}
 	}
+	*/
+
+
+	// second attempt
+	
+
+	for (int i = 0; i < numberOfCircles; i++) {
+
+		for (float thetaDeg = 0.0f; thetaDeg <= 360.0f; thetaDeg += deltaTheta) {
+			// Build a ring
+			MATH::Vec3 circle(r * cos(thetaDeg * DEGREES_TO_RADIANS), r * sin(thetaDeg * DEGREES_TO_RADIANS), 0.0f);
+			// Push the circle point to our vertices array
+			vertices.push_back(circle + (dir * distanceBetweenCircles * i));
+			// The normal of a sphere points outwards from the center position Vec3(x, y, z)
+			normals.push_back(circle); // will fix later
+		}
+
+	}
+
 
 	StoreMeshData(GL_POINTS);
 }
+
+//void GEOMETRY::Cylinder::MakeCircle(float deltaTheta, Vec3& dir_, float distanceBetweenCircles, int currentCircle) {
+//	for (float thetaDeg = 0.0f; thetaDeg <= 360.0f; thetaDeg += deltaTheta) {
+//		// Build a ring
+//		MATH::Vec3 circle(r * cos(thetaDeg * DEGREES_TO_RADIANS), r * sin(thetaDeg * DEGREES_TO_RADIANS), 0.0f);
+//		// Push the circle point to our vertices array
+//		vertices.push_back(circle + (dir_ * distanceBetweenCircles * currentCircle));
+//		// The normal of a sphere points outwards from the center position Vec3(x, y, z)
+//		normals.push_back(circle); // will fix later
+//	}
+//}
+
